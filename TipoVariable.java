82999ce083b3081018a704e2c3153b6cec3a2a10
EusaHack <eusa510@gmail.com>
2023-02-17 15:10:32 -0600

    Add files via upload

---
+++ TipoVariable.java
@@ -0,0 +1,14 @@
+package java_primeros_pasos;
+
+public class TipoVariable {
+	public static void main(String[] args) {
+		for (int numero =1; numero <=100; numero++) {
+			if (numero%3 == 0) {
+				System.out.println(numero);
+			}
+		}
+		//for (int numero = 3; numero <= 100; numero+=3) {
+			//System.out.println(numero);
+		//}
+	}
+}
